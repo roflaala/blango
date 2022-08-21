1b9537ea7d3ba126da252b5b0cba67664aceef92
Abhinav Sharma <aby.now.07@gmail.com>
2022-08-21 19:48:53 +0000

    Finish custom template tags

--- blango/urls.py
+++ blango/urls.py
@@ -18,5 +18,6 @@ from django.urls import path
 from blog import views
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',views.index)
+    path('',views.index),
+    path("post/<slug>/", views.post_detail, name="blog-post-detail")
 ]
--- blog/templatetags/blog_extras.py
+++ blog/templatetags/blog_extras.py
@@ -3,12 +3,13 @@ from django import template
 from django.utils.html import escape
 from django.utils.safestring import mark_safe
 from django.utils.html import format_html
+from blog.models import Post
 user_model = get_user_model()
 register = template.Library()
 
 
 @register.filter
-def author_details(author):
+def author_details(author,current_user=None):
     if not isinstance(author, user_model):
         # return empty string as safe default
         return ""
@@ -29,3 +30,30 @@ def author_details(author):
         suffix = ""
 
     return format_html('{}{}{}', prefix, name, suffix)
+
+@register.simple_tag
+def row():
+    return format_html('<div class="row">')
+
+
+@register.simple_tag
+def endrow():
+    return format_html("</div>")
+
+@register.simple_tag
+def row(extra_classes=""):
+    return format_html('<div class="row {}">', extra_classes)
+
+@register.simple_tag
+def col(extra_classes=""):
+    return format_html('<div class="col {}">', extra_classes)
+
+
+@register.simple_tag
+def endcol():
+    return format_html("</div>")
+
+@register.inclusion_tag("blog/post-list.html")
+def recent_posts(post):
+    posts = Post.objects.exclude(pk=post.pk)
+    return {"title": "Recent Posts", "posts": posts}
